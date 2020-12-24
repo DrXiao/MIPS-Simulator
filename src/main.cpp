3ca3f2c09b9b1be9c0046fc9067f62af1a725c85
Dr.Xiao <zxc71230@gmail.com>
2020-12-24 14:44:54 +0800

    C++ style MIPS Simulator

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,34 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
 #define PAUSE                                                                  \
     printf("Press any key to continue...");                                    \
     fgetc(stdin);
 
-static uint32_t mipsRegisters[32] = {0};
-static uint32_t memory[32] = {0};
+static int mipsRegisters[32] = {0};
+static int memory[32] = {0};
 
 int main(void) {
 
-    for(int indexOfReg_Mem = 0 ; indexOfReg_Mem < 32  ; indexOfReg_Mem++) {
-        mipsRegisters[indexOfReg_Mem] = 1;
-        memory[indexOfReg_Mem] = 1;
-    }
-    mipsRegisters[0] = 0;
-    FILE *mipsIns = fopen("memory.txt", "r");
-    char inputBuf[30] = "";
-    char instruction[4][10] = {"", "", "", ""};
-    char *token = NULL;
-    while (fgets(inputBuf, 30, mipsIns) != NULL) {
-        int index = 0;
-        char *assignPtr = instruction[index++];
-        token = strtok(inputBuf, " ,\n");
-        while (token != NULL) {
-            strcpy(assignPtr, token);
-            assignPtr = instruction[index++];
-            token = strtok(NULL, " ,\n");
+    string instruction;
+    string insToken[4];
+    
+    fstream mipsIns;
+    mipsIns.open("memory.txt", ios::in);
+
+    while(getline(mipsIns, instruction)) {
+        stringstream ss(instruction);
+        for(int tokens = 0 ; tokens < 4 ; tokens++) {
+            getline(ss, insToken[tokens], ',');
+            cout << insToken[tokens] << " ";
         }
-        printf("%s %s %s %s\n", instruction[0], instruction[1], instruction[2],
-               instruction[3]);
+        cout << endl;
+    } 
 
-        memset(inputBuf, 0, sizeof(inputBuf));
-    }
-    fclose(mipsIns);
-    // PAUSE;
-
+    mipsIns.close();
+    PAUSE;
     return 0;
 }
