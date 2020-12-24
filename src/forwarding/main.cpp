af50e82724224a847c5e4ace82a6dd34a1054d40
Dr.Xiao <zxc71230@gmail.com>
2020-12-24 15:18:29 +0800

    Add tall and forwarding dir

--- src/forwarding/main.cpp
+++ src/forwarding/main.cpp
@@ -13,20 +13,27 @@ static int memory[32] = {0};
 
 int main(void) {
 
+    for (int idxOfReg_Mem = 0; idxOfReg_Mem < 32; idxOfReg_Mem++) {
+        mipsRegisters[idxOfReg_Mem] = memory[idxOfReg_Mem] = 1;
+    }
+    mipsRegisters[0] = 0;
+
     string instruction;
     string insToken[4];
-    
+
     fstream mipsIns;
     mipsIns.open("memory.txt", ios::in);
 
-    while(getline(mipsIns, instruction)) {
+    int cycle = 0;
+    while (getline(mipsIns, instruction)) {
+        cycle += 1;
         stringstream ss(instruction);
-        for(int tokens = 0 ; tokens < 4 ; tokens++) {
+        for (int tokens = 0; tokens < 4; tokens++) {
             getline(ss, insToken[tokens], ',');
             cout << insToken[tokens] << " ";
         }
         cout << endl;
-    } 
+    }
 
     mipsIns.close();
     PAUSE;
---
+++ src/stall/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+#define PAUSE                                                                  \
+    printf("Press any key to continue...");                                    \
+    fgetc(stdin);
+
+static int mipsRegisters[32] = {0};
+static int memory[32] = {0};
+
+int main(void) {
+
+    for (int idxOfReg_Mem = 0; idxOfReg_Mem < 32; idxOfReg_Mem++) {
+        mipsRegisters[idxOfReg_Mem] = memory[idxOfReg_Mem] = 1;
+    }
+    mipsRegisters[0] = 0;
+
+    string instruction;
+    string insToken[4];
+
+    fstream mipsIns;
+    mipsIns.open("memory.txt", ios::in);
+
+    int cycle = 0;
+    while (getline(mipsIns, instruction)) {
+        cycle += 1;
+        stringstream ss(instruction);
+        for (int tokens = 0; tokens < 4; tokens++) {
+            getline(ss, insToken[tokens], ',');
+            cout << insToken[tokens] << " ";
+        }
+        cout << endl;
+    }
+
+    mipsIns.close();
+    PAUSE;
+    return 0;
+}
