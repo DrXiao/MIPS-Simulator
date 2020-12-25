d2a3dbed455e5c203697b48bb8ff4f815997bf18
Teazer0 <jasmine24611755@gmail.com>
2020-12-25 14:24:21 +0800

    move header

--- MIPS-Simulator/src/stall/main.cpp
+++ MIPS-Simulator/src/stall/main.cpp
@@ -1,31 +1,21 @@
+#include "pipelineReg.h"
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include "pipelineReg.h"
 using namespace std;
-
 
 #define PAUSE                                                                  \
     printf("Press any key to continue...");                                    \
     fgetc(stdin);
 
-
 static int mipsRegisters[32] = {0};
 static int memory[32] = {0};
 
-extern IF_ID_Pipeline_Reg IF_ID_Reg;
-extern ID_EX_Pipeline_Reg ID_EX_Reg;
-extern EX_MEM_Pipeline_Reg EX_MEM_Reg;
-extern MEM_WB_Pipeline_Reg MEM_WB_Reg;
 
 
 int main(void) {
-    vector<inst> pipeline;
-    /* 按照規定給 Register賦值  */
-    /* 除了0號暫存器為0         */
-    /* 其他為 1                 */
 
     for (int idxOfReg_Mem = 0; idxOfReg_Mem < 32; idxOfReg_Mem++) {
         mipsRegisters[idxOfReg_Mem] = memory[idxOfReg_Mem] = 1;
@@ -47,9 +37,7 @@ int main(void) {
             for(int i = 0;i<insToken[tokens].length();i++){
                 if(insToken[tokens][i] == ',')
                     insToken[tokens][i]='\0';
-                if (insToken[0] == "lw"){
-                    
-                }
+                if (insToken[0] == "lw"){}
                 else if (insToken[0] == "sw"){}
                 else if (insToken[0] == "add"){}
                 else if (insToken[0] == "sub"){}
